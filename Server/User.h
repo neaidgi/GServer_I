8f5a6d58e082de2fb614cc9989646741af0c5587
EverStay <neaidgi@gmail.com>
2018-08-30 13:17:18 +0900

    User 수정

--- Server/User.h
+++ Server/User.h
@@ -12,7 +12,7 @@
 class Character
 {
 private:
-	char* character_name;		// 캐릭터 이름
+	char* character_name;			// 캐릭터 이름
 	int character_code;				// 코드
 	int	character_str;				// 힘
 	int	character_dex;				// 민
@@ -22,9 +22,9 @@ private:
 	int	character_defensepoint;		// 방어력
 	int	character_speed;			// 이속
 	int	character_growhealth;		// 성장 체력
-	int	character_growstr;		// 성장 힘
-	int	character_growdex;		// 성장 민
-	int	character_growint;		// 성장 지
+	int	character_growstr;			// 성장 힘
+	int	character_growdex;			// 성장 민
+	int	character_growint;			// 성장 지
 
 public:
 	const char* GetCharacter_Name() { return character_name; }
@@ -41,7 +41,28 @@ public:
 	int GetCharacter_GrowDEX() { return character_growdex; }
 	int GetCharacter_GrowINT() { return character_growint; }
 
+	void SetCharacter_Name(char* _character_name) 
+	{ 
+		int len = strlen(_character_name);
+		char* name = new char[len+1];
+		memset(name, 0, len + 1);
+		strcpy_s(name, len,_character_name);
+		character_name = name; 
+	}
+	void SetCharacter_Code(int _character_code) { character_code = _character_code; }
+	void SetCharacter_STR(int _character_str) { character_str = _character_str; }
+	void SetCharacter_DEX(int _character_dex) { character_dex = _character_dex; }
+	void SetCharacter_INT(int _character_int) { character_int = _character_int; }
+	void SetCharacter_Health(int _character_health) { character_health = _character_health; }
+	void SetCharacter_AttackPoint(int _character_attackpoint) { character_attackpoint = _character_attackpoint; }
+	void SetCharacter_DefensePoint(int _character_defensepoint) { character_defensepoint = _character_defensepoint; }
+	void SetCharacter_Speed(int _character_speed) { character_speed = _character_speed; }
+	void SetCharacter_GrowHealth(int _character_growhealth) { character_growhealth = _character_growhealth; }
+	void SetCharacter_GrowSTR(int _character_growstr) { character_growstr = _character_growstr; }
+	void SetCharacter_GrowDEX(int _character_growdex) { character_growdex = _character_growdex; }
+	void SetCharacter_GrowINT(int _character_growint) { character_growint = _character_growint; }
 
+	~Character() { delete[]character_name; }
 };
 
 
