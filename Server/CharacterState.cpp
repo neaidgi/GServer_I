a13ff3d1599234b59611a2d1978789ff1a349cae
khy <khy@KHY-PC>
2018-09-04 05:50:53 +0900

    캐릭터 상태 수정

--- Server/CharacterState.cpp
+++ Server/CharacterState.cpp
@@ -1,34 +1,42 @@
 #include "CharacterState.h"
+#include "CharacterManager.h"
 
 bool CharacterState::Read(User * _user)
 {
+	CharacterManager* charactermanager = CharacterManager::GetInstance();
+	int result;
+
 	switch (state)
 	{
-	case INIT_RECV:
-		//result = loginmanager->logoutMenuChoice(_user);
-		//if (result == RT_LOGINMENU)
-		//{
-		//	state = INIT_SEND;
-		//}
-		//// 수정했음
-		//else if (result == RT_LOGIN)
-		//{
-		//	state = LOGIN_RESULT_SEND;
-		//}
-		//// 수정했음
-		//else if (result == RT_LOGINFAIL)
-		//{
-		//	state = LOGINFAIL_RESULT_SEND;
-		//}
-		//else if (result == RT_JOINMENU)
-		//{
-		//	state = REQUEST_RESULT_SEND;
-		//}
-		//else if (result == RT_USER_DISCONNECT)
-		//{
-		//	return false;
-		//}
+	case CharacterState::CHARACTER_MENU_RECV:
+		result = charactermanager->Character_Init_Choice(_user);
+		if (result == RT_CHARACTER_SLOTRESULT)
+		{
+			state = CHARACTER_SLOT_RESULT_SEND;
+		}
+		else if (result == RT_CHARACTER_ENTERCREATE)
+		{
+			state = CHARACTER_CREATE_MENU_SEND;
+		}
+		else if (result == RT_CHARACTER_ENTERGAME)
+		{
+			state = CHARACTER_ENTER_SEND;
+		}
 		break;
+	case CharacterState::CHARACTER_REQ_RECV:
+		result = charactermanager->Character_Management_Process(_user);
+		if (result == RT_CHARACTER_NICKOVERLAP_TRUE)
+		{
+			state = CHARACTER_REQ_OVLAP_NICK_SEND;
+		}
+		else if (result == RT_CHARACTER_CREATE)
+		{
+			state = CHARACTER_REQ_CHARACTER_SEND;
+		}
+		else if (result == RT_CHARACTER_EXIT)
+		{
+			state = CHARACTER_EXIT_SEND;
+		}
 	default:
 		break;
 	}
@@ -40,6 +48,25 @@ bool CharacterState::Write(User * _user)
 {
 	switch (state)
 	{
+	case CharacterState::CHARACTER_SLOT_RESULT_SEND:
+		state = CHARACTER_MENU_RECV;
+		break;
+	case CharacterState::CHARACTER_CREATE_MENU_SEND:
+		state = CHARACTER_REQ_RECV;
+		break;
+	case CharacterState::CHARACTER_ENTER_SEND:
+		state = CHARACTER_MENU_RECV;
+		//_user->SetState();
+		break;
+	case CharacterState::CHARACTER_REQ_OVLAP_NICK_SEND:
+		state = CHARACTER_REQ_RECV;
+		break;
+	case CharacterState::CHARACTER_REQ_CHARACTER_SEND:
+		state = CHARACTER_MENU_RECV;
+		break;
+	case CharacterState::CHARACTER_EXIT_SEND:
+		state = CHARACTER_MENU_RECV;
+		break;
 	default:
 		break;
 	}
