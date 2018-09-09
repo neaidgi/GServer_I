22d510a637b306cd06a5ef4a4bdcaa0fb204450f
khy <khy@KHY-PC>
2018-09-09 21:45:42 +0900

    SQL 수정

--- DB/SQLFile.sql
+++ DB/SQLFile.sql
@@ -1,20 +1,33 @@
+create database userinfo;
+
+use userinfo;
+
+create table info
+(
+	id varchar(20) not null primary key,
+	pw varchar(20) not null,
+    nick varchar(20) not null
+);
+
 use userinfo;
 
 create table CharacterInfo
 (
 	character_code INT NOT NULL primary key,
-    character_name VARCHAR(20) NOT NULL,
+    character_jobname VARCHAR(20) NOT NULL,
     character_str INT NOT NULL,
     character_dex INT NOT NULL,
     character_int INT NOT NULL,
     character_health INT NOT NULL,
+    character_mana INT NOT NULL,
     character_attackpoint INT,
     character_defensepoint INT,
-    character_speed FLOAT NOT NULL,
-    character_growthhealth INT NOT NULL,
-    character_growthstr INT NOT NULL,
-    character_growthdex INT NOT NULL,
-    character_growthint INT NOT NULL
+    character_speed INT NOT NULL,
+    character_growhealth INT NOT NULL,
+    character_growmana INT NOT NULL,
+    character_growstr INT NOT NULL,
+    character_growdex INT NOT NULL,
+    character_growint INT NOT NULL
 );
 
 use userinfo;
@@ -32,40 +45,51 @@ create table ItemInfo
 
 use userinfo;
 
+/* 
+character_code = 0000 00 00 00 00 00 0000
+*/
+create table CharacterPos
+(
+	character_code INT NOT NULL PRIMARY KEY,
+	character_pos_x FLOAT,
+	chatacter_pos_y FLOAT,
+	character_pos_z FLOAT
+);
+
+use userinfo;
+
 create table UserCharacterInfo
 (
 	user_id VARCHAR(20) NOT NULL,
     
-	charactercode_first INT,
-    charactername_first VARCHAR(20),
-    characternick_first VARCHAR(20),
-    characterlevel_first INT,
+	character_origin_code_first INT,
+    character_jobname_first VARCHAR(20),
+    character_nickname_first VARCHAR(20),
+    character_level_first INT,
+    character_code_first INT,
     
-    charactercode_seconde INT,
-    charactername_seconde VARCHAR(20),
-    characternick_seconde VARCHAR(20),
-    characterlevel_seconde INT,
+    character_origin_code_second INT,
+    character_jobname_second VARCHAR(20),
+    character_nickname_second VARCHAR(20),
+    character_level_second INT,
+    character_code_second INT,
     
-    charactercode_third INT,
-    charactername_third VARCHAR(20),
-    characternick_third VARCHAR(20),
-    characterlevel_third INT,
+    character_origin_code_third INT,
+    character_jobname_third VARCHAR(20),
+    character_nickname_third VARCHAR(20),
+    character_level_third INT,
+    character_code_third INT,
     
     PRIMARY KEY(user_id),
-	FOREIGN KEY(user_id) REFERENCES info(id),
-    FOREIGN KEY(charactercode_first) REFERENCES CharacterInfo(character_code),
-    FOREIGN KEY(charactercode_seconde) REFERENCES CharacterInfo(character_code),
-    FOREIGN KEY(charactercode_third) REFERENCES CharacterInfo(character_code)
+	FOREIGN KEY(user_id) REFERENCES info(id)
 );
 
 
 use userinfo;
 
-INSERT INTO CharacterInfo VALUES (1,'Tanker',50,10,10,150,0,0,0.8,20,10,0,100),
-								 (2,'Warrior',10,50,10,100,0,0,1.0,10,20,0,75),
-								 (3,'Magician',10,10,50,50,0,0,1.2,5,5,20,50)
-               
-               
+INSERT INTO CharacterInfo VALUES (1000,'Tanker',15,10,10,150,100,0,0,100,10,5,5,100,50),
+								 (2000,'Warrior',10,15,10,100,100,0,0,100,5,10,5,50,50),
+								 (3000,'Magician',10,10,15,100,150,0,0,100,5,5,10,50,100);
                
 use userinfo;
 
@@ -73,4 +97,12 @@ INSERT INTO ItemInfo VALUES (1,'HealthPotion',1,1,1,'체력을 50 회복해준�
                             (2,'Axe',2,1,2,'탱커의 기본무기',1),
                             (3,'Sword',2,1,2,'전사의 기본무기',2),
                             (4,'Staff',2,1,2,'마법사의 기본무기',3),
-                            (5,'ClothAmor',3,1,3,'기본 천갑옷',4)
+                            (5,'ClothAmor',3,1,3,'기본 천갑옷',4);
+
+
+
+drop table usercharacterinfo;
+drop table CharacterPos;
+drop table characterinfo;
+drop table iteminfo;
+
