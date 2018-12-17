d5d7ab501964f0f73f1106f79e88189e8b0d9f47
EverStay <neaidgi@gmail.com>
2018-12-17 11:14:49 +0900

    SpawnData 주석 추가

--- Server/SpawnData.cpp
+++ Server/SpawnData.cpp
@@ -14,6 +14,7 @@ const Vector3 * SpawnData::Character_Spawn_PosData()
 	return character_spawn_pos;
 }
 
+// 플레이어 스폰위치 DB에서 받아옴
 bool SpawnData::Player_SpawnPos_Load()
 {
 	DBManager* db = DBManager::GetInstance();
@@ -22,12 +23,9 @@ bool SpawnData::Player_SpawnPos_Load()
 	{
 		return false;
 	}
-	else
-	{
-
-	}
 }
 
+// 스폰데이터 클래스 초기화
 bool SpawnData::Init_Spawn_Data()
 {
 	if (Player_SpawnPos_Load() == false)
