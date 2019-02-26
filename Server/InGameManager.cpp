1f3369fcc0b24043c65268dbf15aa8284d2a108a
EverStay <neaidgi@gmail.com>
2019-02-26 17:43:43 +0900

    접속한 유저정보 로그 추가

--- Server/InGameManager.cpp
+++ Server/InGameManager.cpp
@@ -140,16 +140,23 @@ void InGameManager::User_Pack_PlayerPosData(User * _user, char* _data, int& _dat
 
 	memset(ptr, 0, BUFSIZE);
 
+	char msg[BUFSIZE];
+	memset(msg, 0, sizeof(msg));
+
 	// 코드 사이즈
 	len = strlen(_user->GetCurCharacter()->GetCharacter_Code());
 	memcpy(ptr, &len, sizeof(int));
 	ptr += sizeof(int);
 	size += sizeof(int);
 
+	sprintf(msg, "접속한 유저 데이터 :: 코드사이즈: %d ", len);
+
 	// 코드
 	memcpy(ptr, _user->GetCurCharacter()->GetCharacter_Code(), len);
 	ptr += len;
 	size += len;
+
+	sprintf(msg, "코드: [%s] ", _user->GetCurCharacter()->GetCharacter_Code());
 
 	// 직업 
 	int jobcode = _user->GetCurCharacter()->GetCharacter_JobCode();
@@ -157,28 +164,41 @@ void InGameManager::User_Pack_PlayerPosData(User * _user, char* _data, int& _dat
 	ptr += sizeof(int);
 	size += sizeof(int);
 
+	sprintf(msg, "직업코드: [%d] ", jobcode);
+
 	// 닉네임 사이즈
 	len = strlen(_user->GetCurCharacter()->GetCharacter_Name());
 	memcpy(ptr, &len, sizeof(int));
 	ptr += sizeof(int);
 	size += sizeof(int);
 
+	sprintf(msg, "닉네임사이즈: [%d] ", len);
+
 	// 닉네임
 	memcpy(ptr, _user->GetCurCharacter()->GetCharacter_Name(), len);
 	ptr += len;
 	size += len;
+
+	sprintf(msg, "닉네임: [%s]\n", _user->GetCurCharacter()->GetCharacter_Name());
 
 	// 위치
 	memcpy(ptr, &_user->GetCurCharacter()->GetPosition(), sizeof(Vector3));
 	ptr += sizeof(Vector3);
 	size += sizeof(Vector3);
 
+	sprintf(msg, "위치: [%f] [%f] [%f] ", _user->GetCurCharacter()->GetPosition().x, _user->GetCurCharacter()->GetPosition().y, _user->GetCurCharacter()->GetPosition().z);
+
 	// 회전
 	memcpy(ptr, &_user->GetCurCharacter()->GetRotation(), sizeof(Vector3));
 	ptr += sizeof(Vector3);
 	size += sizeof(Vector3);
 
+	sprintf(msg, "회전: [%f] [%f] [%f] ", _user->GetCurCharacter()->GetRotation().x, _user->GetCurCharacter()->GetRotation().y, _user->GetCurCharacter()->GetRotation().z);
+
 	_datasize = size;
+
+	// 메세지
+	MsgManager::GetInstance()->DisplayMsg("INFO", msg);
 }
 
 // 이동 완료 요청
